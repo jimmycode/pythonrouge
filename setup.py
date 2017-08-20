5f74f92893582a27645e03e1b986b79b405a2778
WU Yuxiang <ywubw@qycpu10.cse.ust.hk>
2017-08-20 20:56:33 +0800

    Cleaned the package.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="pythonrouge",
@@ -7,23 +7,16 @@ setup(
     url="http://github.com/jimmycode/pythonrouge",
     author="Jimmy WU",
     author_email="topcoderjimmy@gmail.com",
-    keywords=["NL", "CL", "natural language processing", "computational linguistics", "summarization"],
+    keywords=[
+        "NL", "CL", "natural language processing", "computational linguistics",
+        "summarization"
+    ],
     packages=["pythonrouge"],
-    package_data={
-        'pythonrouge': ['RELEASE-1.5.5/*.*',
-              'RELEASE-1.5.5/data/smart_common_words.txt',
-              'RELEASE-1.5.5/data/WordNet-1.6.exc.db',
-              'RELEASE-1.5.5/data/WordNet-2.0.exc.db',
-              'RELEASE-1.5.5/data/WordNet-1.6-Exceptions/*.*',
-              'RELEASE-1.5.5/data/WordNet-2.0-Exceptions/*.*',
-              ],
-        },
     classifiers=[
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
         "Topic :: Text Processing :: Linguistic"
-        ],
+    ],
     license="LICENCE.txt",
-    long_description=open("README.md").read(),
-)
+    long_description=open("README.md").read(),)
